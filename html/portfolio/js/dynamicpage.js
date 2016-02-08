8ffcb5e8a646d679f54f6b2081298efd05fc4216
kathrynfortner <kfortner@uoregon.edu>
2016-02-07 16:43:49 -0800

    portfolio update

--- html/portfolio/js/dynamicpage.js
+++ html/portfolio/js/dynamicpage.js
@@ -1,9 +1,41 @@
-function getContent(id)
+function getContent1()
 {
    $.ajax({
 
      type: "GET",
      url: 'wif/wif1.html',
+     data: "id=" + id, // appears as $_GET['id'] @ your backend side
+     success: function(data) {
+           // data is ur summary
+          $('#content').html(data);
+     }
+
+   });
+
+}
+
+function getContent2()
+{
+   $.ajax({
+
+     type: "GET",
+     url: 'wif/wif2.html',
+     data: "id=" + id, // appears as $_GET['id'] @ your backend side
+     success: function(data) {
+           // data is ur summary
+          $('#content').html(data);
+     }
+
+   });
+
+}
+
+function getContent3()
+{
+   $.ajax({
+
+     type: "GET",
+     url: 'wif/wif3.html',
      data: "id=" + id, // appears as $_GET['id'] @ your backend side
      success: function(data) {
            // data is ur summary

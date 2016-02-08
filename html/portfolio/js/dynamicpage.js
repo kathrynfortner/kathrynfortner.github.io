b9bb2ba5561374581338b5f0e75aa561974df261
kathrynfortner <kfortner@uoregon.edu>
2016-02-07 16:45:43 -0800

    test

--- html/portfolio/js/dynamicpage.js
+++ html/portfolio/js/dynamicpage.js
@@ -1,41 +1,9 @@
-function getContent1()
+function getContent()
 {
    $.ajax({
 
      type: "GET",
      url: 'wif/wif1.html',
-     data: "id=" + id, // appears as $_GET['id'] @ your backend side
-     success: function(data) {
-           // data is ur summary
-          $('#content').html(data);
-     }
-
-   });
-
-}
-
-function getContent2()
-{
-   $.ajax({
-
-     type: "GET",
-     url: 'wif/wif2.html',
-     data: "id=" + id, // appears as $_GET['id'] @ your backend side
-     success: function(data) {
-           // data is ur summary
-          $('#content').html(data);
-     }
-
-   });
-
-}
-
-function getContent3()
-{
-   $.ajax({
-
-     type: "GET",
-     url: 'wif/wif3.html',
      data: "id=" + id, // appears as $_GET['id'] @ your backend side
      success: function(data) {
            // data is ur summary

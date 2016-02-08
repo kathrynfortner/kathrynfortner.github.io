57cf8f13fcb21bca80b07603e2a757d9c72dab8a
kathrynfortner <kfortner@uoregon.edu>
2016-02-07 18:59:21 -0800

    updates

--- js/creative.js
+++ js/creative.js
@@ -61,7 +61,7 @@ function submitForm(){
  
     $.ajax({
         type: "POST",
-        url: "php/form-process.php",
+        url: "../php/process.php",
         data: "name=" + name + "&email=" + email + "&message=" + message,
         success : function(text){
             if (text == "success"){

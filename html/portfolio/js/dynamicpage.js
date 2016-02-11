a159c460875ac4a18cb982762e75ecab4cf62dbd
kathrynfortner <kfortner@uoregon.edu>
2016-02-11 01:25:57 -0800

    wif portfolio

--- html/portfolio/js/dynamicpage.js
+++ html/portfolio/js/dynamicpage.js
@@ -1,15 +1,30 @@
-function getContent()
-{
-   $.ajax({
-
-     type: "GET",
-     url: 'wif/wif1.html',
-     data: "id=" + id, // appears as $_GET['id'] @ your backend side
-     success: function(data) {
-           // data is ur summary
-          $('#content').html(data);
-     }
-
-   });
-
-}
+$(document).ready(function() {
+    "use strict"; 
+    // Check for hash value in URL
+    var hash = window.location.hash.substr(1);
+    var href = $('#projects a').each(function(){
+        var href = $(this).attr('href');
+        if(hash===href.substr(0,href.length-5)){
+            var toLoad = hash+'.html #content';
+            $('#content').load(toLoad);
+        } 
+    });
+     
+    $('#projects a').click(function(){
+     
+    var toLoad = $(this).attr('href')+' #content';
+    $('#content').hide('fast',loadContent);
+    window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
+    function loadContent() {
+        $('#content').load(toLoad,'',showNewContent());
+    }
+    function showNewContent() {
+        $('#content').show('normal',hideLoader());
+    }
+    function hideLoader() {
+        $('#load').fadeOut('normal');
+    }
+    return false;
+     
+    });
+});
